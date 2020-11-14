7d222808a79f7fd9fc27b864ab07b4752f54fdf9
B3W <westieberg@gmail.com>
2020-11-14 14:06:00 -0600

    Cleaner, simpler solution. Works with any font type.

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import resizabletext as rt
 
 if __name__ == '__main__':
     root = tk.Tk()
+    root.geometry('200x100')
     root.minsize(width=100, height=100)
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=0)
 
-    # NOTE Need monospaced font
-    fixed_font = font.Font(family="Courier", size=12)
+    # NOTE Can use any font
+    custom_font = font.Font(family="Times New Roman", size=12)
     text = rt.ResizableText(root,
-                            fixed_font,
+                            font=custom_font,
                             relief=tk.FLAT,
                             wrap='word',
                             highlightbackground="light grey",
@@ -21,6 +22,6 @@ if __name__ == '__main__':
     text.insert(1.0, 'This is text that will get resized as the widget does')
     text.grid(column=0, row=0, sticky=tk.EW)
 
-    # NOTE Must use configure event of master to resize widget width
+    # NOTE Must use configure event of parent to notify widget of resize
     root.bind('<Configure>', text.on_configure)
     root.mainloop()
--- resizabletext.py
+++ resizabletext.py
@@ -1,60 +1,29 @@
 '''Module providing resizable text widget'''
 import tkinter as tk
-import math
 
 
 class ResizableText(tk.Text):
-    def __init__(self, master, fixed_font, *args, **kwargs):
-        tk.Text.__init__(self, master, font=fixed_font, *args, **kwargs)
-        self.font = fixed_font
+    def __init__(self, master, *args, **kwargs):
+        tk.Text.__init__(self, master, *args, **kwargs)
         self.line_count = 0
         self._resizing = False
-
-        # Pixel width to font width conversion number
-        self._pixel_unit = self.font.measure('0')
 
         self.bind('<<Modified>>', self.resize)
 
     def resize(self, event=None):
-        widget_width = 0
-        resized_line_count = 0
-        max_width = self.cget('width')
-        lines = self.get('1.0', 'end-1c').splitlines()
-
-        for line in lines:
-            line_width = len(line)
-
-            if line_width > max_width:
-                widget_width = max_width
-
-                # Calculate number of times line will wrap
-                resized_line_count += line_width / max_width
-
-            else:
-                widget_width = max(widget_width, line_width)
-                resized_line_count += 1
-
-        resized_line_count = math.ceil(resized_line_count)
+        # Count number of lines needed to display text
+        resized_line_count = self.count(1.0, tk.END, 'displaylines')[0]
 
         # Only configure height if it has changed
-        if self.line_count != resized_line_count:
+        if resized_line_count != self.line_count:
+            self.configure(height=resized_line_count)
             self.line_count = resized_line_count
-            self.configure(height=self.line_count)
 
         self._resizing = False
 
     def on_configure(self, event):
         # NOTE 'event.width' is the pixel width of the text
-        if event.width <= 1:  # Ignore calls with no width
-            return
-
         # Only resize every ~200ms
         if not self._resizing:
             self._resizing = True
-
-            # Bit of a hack. Text widgets determine their width based on the
-            # pixel size of a '0'. Convert the given pixel width to font width
-            converted_width = int(event.width / self._pixel_unit)
-            self.configure(width=converted_width)
-
             self.after(200, self.resize)
